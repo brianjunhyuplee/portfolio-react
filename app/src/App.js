d5b411f551451de533a0229e86d006d4434597d5
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-11-18 18:36:53 -0800

    footer

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return(
   <Router>
   <Navbar />
+  <Footer />
   </Router>
   );
 }
--- app/src/components/Footer/index.js
+++ app/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer>
+        <footer className="footer">
             <span>Copyright brianjunhyuplee</span>
         </footer>
     )

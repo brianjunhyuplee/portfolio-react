47304060f9cbac8f8c313f347e1f649f232edebb
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-11-18 18:32:15 -0800

    Navbar working

--- app/src/App.js
+++ app/src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
+  return(
   <Router>
   <Navbar />
   </Router>
+  );
 }
 
 export default App;
--- app/src/components/NavBar/index.js
+++ app/src/components/NavBar/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./style.css"
 function Navbar() {
     return(
         <nav class="navbar navbar-dark bg-dark">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav mr-auto">
                 <li class="navbar-item">
                     <Link to="/">Home</Link>
                 </li>
                 <li class="navbar-item">
                     <Link to="/portfolio">Portfolio</Link>
                 </li>
-                <li>
+                <li class="navbar-item">
                     <Link to="/contact">Contact</Link>
                 </li>
                 

e5ecaa945ccd4fd95b4421a770c1a68d4575b8ea
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-11-18 18:49:14 -0800

    Home page

---
+++ app/src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return(
+        <div className="container">
+            <div className="row">
+                <h1 style={{textAlign : "center"}}>About Me</h1>
+            </div>
+        </div>
+    )
+}

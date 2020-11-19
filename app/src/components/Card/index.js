decf51e2df2cae735959fd4141bc45fba6669c0c
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-11-18 18:42:36 -0800

    card

--- app/src/components/Card/index.js
+++ app/src/components/Card/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-function Card() {
-    
+function Card(props) {
+    return(
+        <div className="card">
+            <div className="card-header">{props.title}</div>
+            <div className="card-body">
+               <p>{props.body}</p> 
+            </div>
+            
+        </div>
+    )
 }
+
+export default Card;

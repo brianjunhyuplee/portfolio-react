f38a7e2eddc90e843f27f069f6ac2665a8f4c75a
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-11-18 18:09:22 -0800

    Navbar template

--- app/src/components/NavBar/index.js
+++ app/src/components/NavBar/index.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return{
-        <nav className="nav">
-        
-    }
+    return(
+        <nav class="navbar navbar-dark bg-dark">
+            <ul className="navbar-nav">
+                <li class="navbar-item">
+                    <Link to="/">Home</Link>
+                </li>
+                <li class="navbar-item">
+                    <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                
+            </ul>
+        </nav>
+    )
 }
